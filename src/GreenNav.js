c8078d9e6529ff5301c55f46bb5d1a6ff5c95f51
Fabian Bormann <fabian_mb@gmx.de>
2016-11-24 16:06:40 +0100

    add bootstrap react

---
+++ src/GreenNav.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+
+class GreenNav extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">GreenNav</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">Link</NavItem>
+              <NavItem eventKey={2} href="#">Link</NavItem>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">Link Right</NavItem>
+              <NavItem eventKey={2} href="#">Link Right</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default GreenNav;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GreenNav from './GreenNav';
+
+ReactDOM.render(
+  <GreenNav />,
+  document.getElementById('root')
+);
