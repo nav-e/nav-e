d7f946a253312d4a353cbdd0205086a16ade9ba4
tom3012 <thomas83ort@gmail.com>
2016-12-12 01:14:16 +0100

    Added GreenNav Material-UI Theme.

--- src/index.js
+++ src/index.js
@@ -3,21 +3,16 @@ import ReactDOM from 'react-dom';
 import GreenNav from './GreenNav';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import { green700 } from 'material-ui/styles/colors';
+import gnMuiTheme from './styles/gnMuiTheme'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-const muiTheme = getMuiTheme({
-  slider: {
-    selectionColor: green700
-  },
-});
 
 ReactDOM.render(
-  <MuiThemeProvider muiTheme={muiTheme}>
+  <MuiThemeProvider muiTheme={getMuiTheme(gnMuiTheme)}>
     <GreenNav />
   </MuiThemeProvider>,
   document.getElementById('root')
