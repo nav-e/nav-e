0ff0054f57b09210284de39df8ac3e3080f85d20
tom3012 <thomas83ort@gmail.com>
2017-02-05 01:36:08 +0100

    Fixed linting errors.

--- src/components/GreenNavMap.js
+++ src/components/GreenNavMap.js
@@ -1,37 +1,37 @@
 import React, { Component, PropTypes } from 'react';
 import { red600 } from 'material-ui/styles/colors';
 
-var ol = require('openlayers');
+const ol = require('openlayers');
 
 const styles = {
   container: {
     height: '100%',
     width: '100%'
   }
-}
+};
 
 export default class GreenNavMap extends Component {
 
   constructor(props) {
     super(props);
 
-    var lineStyle = new ol.style.Style({
+    const lineStyle = new ol.style.Style({
       stroke: new ol.style.Stroke({
         color: red600,
         width: 4
       })
     });
 
-    var routeLayer = new ol.layer.Vector({
+    const routeLayer = new ol.layer.Vector({
       style: lineStyle
     });
 
-    var osmLayer = new ol.layer.Tile({
+    const osmLayer = new ol.layer.Tile({
       visible: true,
       source: new ol.source.OSM()
-    })
+    });
 
-    var googleLayer = new ol.layer.Tile({
+    const googleLayer = new ol.layer.Tile({
       visible: false,
       source: new ol.source.OSM({
         url: 'http://mt{0-3}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
@@ -43,38 +43,40 @@ export default class GreenNavMap extends Component {
     });
 
     // Traffic layer
-    var computeQuadKey = function(x, y, z) {
-      var quadKeyDigits = [];
-      for (var i = z; i > 0; i--) {
-          var digit = 0;
-          var mask = 1 << (i - 1);
-          if ((x & mask) !== 0)
-              digit++;
-          if ((y & mask) !== 0)
-              digit +=  2;
-          quadKeyDigits.push(digit);
+    const computeQuadKey = (x, y, z) => {
+      const quadKeyDigits = [];
+      for (let i = z; i > 0; i -= 1) {
+        let digit = 0;
+        const mask = 1 << (i - 1);
+        if ((x & mask) !== 0) {
+          digit += 1;
+        }
+        if ((y & mask) !== 0) {
+          digit += 2;
+        }
+        quadKeyDigits.push(digit);
       }
       return quadKeyDigits.join('');
     };
-    var trafficLayer = new ol.layer.Tile({
-        visible: false,
-        source: new ol.source.XYZ({
-            maxZoom: 19,
-            tileUrlFunction(tileCoord, pixelRatio, projection) {
-                var z = tileCoord[0];
-                var x = tileCoord[1];
-                var y = -tileCoord[2] - 1;
-                return "http://t0.tiles.virtualearth.net/tiles/t" + computeQuadKey(x, y, z) + ".png";
-            }
-        })
+    const trafficLayer = new ol.layer.Tile({
+      visible: false,
+      source: new ol.source.XYZ({
+        maxZoom: 19,
+        tileUrlFunction(tileCoord, pixelRatio, projection) {
+          const z = tileCoord[0];
+          const x = tileCoord[1];
+          const y = -tileCoord[2] - 1;
+          return `http://t0.tiles.virtualearth.net/tiles/t${computeQuadKey(x, y, z)}.png`;
+        }
+      })
     });
 
     // Temperature layer
-    var temperatureLayer =  new ol.layer.Tile({
+    const temperatureLayer = new ol.layer.Tile({
       opacity: 0.3,
       visible: false,
       source: new ol.source.OSM({
-        url: "http://{s}.tile.openweathermap.org/map/temp/{z}/{x}/{y}.png",
+        url: 'http://{s}.tile.openweathermap.org/map/temp/{z}/{x}/{y}.png',
         attribution: 'Map data © OpenWeatherMap',
         crossOrigin: null,
         maxZoom: 18
@@ -82,18 +84,18 @@ export default class GreenNavMap extends Component {
     });
 
     // Wind layer
-    var windLayer =  new ol.layer.Tile({
+    const windLayer = new ol.layer.Tile({
       opacity: 0.3,
       visible: false,
       source: new ol.source.OSM({
-        url: "http://{s}.tile.openweathermap.org/map/wind/{z}/{x}/{y}.png",
+        url: 'http://{s}.tile.openweathermap.org/map/wind/{z}/{x}/{y}.png',
         attribution: 'Map data © OpenWeatherMap',
         crossOrigin: null,
         maxZoom: 18
       })
     });
 
-    var map = new ol.Map({
+    const map = new ol.Map({
       layers: [osmLayer, googleLayer, routeLayer, trafficLayer, temperatureLayer, windLayer],
       view: new ol.View({
         center: ol.proj.fromLonLat([this.props.longitude, this.props.latitude]),
@@ -102,28 +104,24 @@ export default class GreenNavMap extends Component {
     });
 
     this.state = {
-      map : map,
+      map,
       traffic: false,
       temperature: false,
       wind: false
-    }
+    };
   }
 
-  componentDidMount () {
-    this.state.map.setTarget(this.refs.map)
-    window.addEventListener("resize", this.updateSize);
+  componentDidMount() {
+    this.state.map.setTarget(this.map);
+    window.addEventListener('resize', this.updateSize);
   }
 
   componentWillUnmount() {
-    window.removeEventListener("resize", this.updateSize);
+    window.removeEventListener('resize', this.updateSize);
   }
 
-  updateSize = () => {
-    this.state.map.updateSize()
-  }
-
-  setMapType = mapType => {
-    if(mapType === 0) {
+  setMapType = (mapType) => {
+    if (mapType === 0) {
       this.state.map.getLayers().getArray()[0].setVisible(true);
       this.state.map.getLayers().getArray()[1].setVisible(false);
     }
@@ -133,35 +131,20 @@ export default class GreenNavMap extends Component {
     }
   }
 
-  toggleTraffic = () => {
-    this.setState({traffic: !this.state.traffic})
-    this.state.map.getLayers().getArray()[3].setVisible(!this.state.traffic);
-  }
-
-  toggleTemperature = () => {
-    this.setState({temperature: !this.state.temperature})
-    this.state.map.getLayers().getArray()[4].setVisible(!this.state.temperature);
-  }
-
-  toggleWind = () => {
-    this.setState({wind: !this.state.wind})
-    this.state.map.getLayers().getArray()[5].setVisible(!this.state.wind);
-  }
-
-  setRoute = route => {
-    var coords = [];
-    route.forEach(point => {
+  setRoute = (route) => {
+    const coords = [];
+    route.forEach((point) => {
       coords.push([point.lon, point.lat]);
     });
-    var lineString = new ol.geom.LineString(coords);
+    const lineString = new ol.geom.LineString(coords);
     lineString.transform('EPSG:4326', 'EPSG:3857');
 
-    var feature = new ol.Feature({
+    const feature = new ol.Feature({
       geometry: lineString,
       name: 'Line'
     });
 
-    var source = new ol.source.Vector({
+    const source = new ol.source.Vector({
       features: [feature]
     });
 
@@ -170,9 +153,28 @@ export default class GreenNavMap extends Component {
     this.state.map.getLayers().getArray()[2].setSource(source);
   }
 
+  toggleTraffic = () => {
+    this.setState({ traffic: !this.state.traffic });
+    this.state.map.getLayers().getArray()[3].setVisible(!this.state.traffic);
+  }
+
+  toggleTemperature = () => {
+    this.setState({ temperature: !this.state.temperature });
+    this.state.map.getLayers().getArray()[4].setVisible(!this.state.temperature);
+  }
+
+  toggleWind = () => {
+    this.setState({ wind: !this.state.wind });
+    this.state.map.getLayers().getArray()[5].setVisible(!this.state.wind);
+  }
+
+  updateSize = () => {
+    this.state.map.updateSize();
+  }
+
   render() {
     return (
-      <div style={styles.container} ref="map"></div>
+      <div style={styles.container} ref={c => (this.map = c)} />
     );
   }
 }
@@ -181,7 +183,7 @@ GreenNavMap.propTypes = {
   longitude: PropTypes.number,
   latitude: PropTypes.number,
   zoom: PropTypes.number
-}
+};
 
 GreenNavMap.defaultProps = {
   longitude: 11.566,
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import {Tabs, Tab} from 'material-ui/Tabs';
+import { Tabs, Tab } from 'material-ui/Tabs';
 import AutoComplete from 'material-ui/AutoComplete';
 import Slider from 'material-ui/Slider';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -7,7 +7,7 @@ import FlatButton from 'material-ui/FlatButton';
 import FontIcon from 'material-ui/FontIcon';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-import Paper from 'material-ui/Paper'
+import Paper from 'material-ui/Paper';
 
 const styles = {
   container: {
@@ -36,7 +36,7 @@ const styles = {
   slider: {
     width: '300px'
   },
-}
+};
 
 export default class Menu extends Component {
 
@@ -49,64 +49,66 @@ export default class Menu extends Component {
       dataSource: [],
       route: [],
       stopovers: 0
+    };
+  }
+
+  getVehicles = () => [
+    'Fiat Fiorino',
+    'Smart Roadster',
+    'Sam',
+    'Citysax',
+    'MUTE',
+    'Spyder-S',
+    'Think',
+    'Luis',
+    'STROMOS',
+    'Karabag Fiat 500E',
+    'Lupower Fiat 500E'
+  ]
+
+  getRoute = () => {
+    if (this.state.route.every(waypoint => waypoint !== '')) {
+      this.props.getRoute(this.state.route);
+    }
+    else {
+      // TODO: implement notifications (Thomas GSoC Project - see polymer reference branch)
+      alert('Please select a start and destination from the suggestions');
     }
   }
 
-  getVehicles = () => {
-    return ["Fiat Fiorino",
-            "Smart Roadster",
-            "Sam",
-            "Citysax",
-            "MUTE",
-            "Spyder-S",
-            "Think",
-            "Luis",
-            "STROMOS",
-            "Karabag Fiat 500E",
-            "Lupower Fiat 500E"
-    ];
-  }
-
-  toggle = callback => {
-    this.setState({open: !this.state.open}, callback);
-  }
-
-  vehicleChange = (event, index, value) => {
-    this.setState({vehicle: value});
-  }
-
-  handleUpdate = address => {
-    if(this.state.xhr.readyState !== 0 || this.state.xhr.readyState !== 4) {
+  handleUpdate = (address) => {
+    if (this.state.xhr.readyState !== 0 || this.state.xhr.readyState !== 4) {
       this.state.xhr.abort();
     }
 
     this.state.xhr.onreadystatechange = () => {
       if (this.state.xhr.readyState === 4 && this.state.xhr.status === 200) {
-        let places = JSON.parse(this.state.xhr.responseText).map(place => {
-          return {'text': place.display_name, 'data': {
+        const places = JSON.parse(this.state.xhr.responseText).map(place => (
+          {
+            text: place.display_name,
+            data: {
               osm_id: place.osm_id,
               longitude: place.lon,
               latitude: place.lat
-            }}
-        });
+            }
+          }
+        ));
         this.setState({
           dataSource: places
         });
       }
     };
 
-    this.state.xhr.open('GET', this.props.autoCompleteAddress+'search/'+address, true);
+    this.state.xhr.open('GET', `${this.props.autoCompleteAddress}search/${address}`, true);
     this.state.xhr.send();
   }
 
-  getRoute = () => {
-    if(this.state.route.every(waypoint => waypoint !== '')) {
-      this.props.getRoute(this.state.route);
-    }
-    else {
-      //TODO: implement notifications (Thomas GSoC Project - see polymer reference branch)
-      alert('Please select a start and destination from the suggestions');
-    }
+  toggle = (callback) => {
+    this.setState({ open: !this.state.open }, callback);
+  }
+
+  vehicleChange = (event, index, value) => {
+    this.setState({ vehicle: value });
   }
 
   render() {
@@ -116,8 +118,12 @@ export default class Menu extends Component {
     };
 
     return (
-      <Paper style={this.state.open ? styles.container : {display: 'none'}} zDepth={5}>
-        <Tabs contentContainerStyle={styles.tabsContainer} inkBarStyle={styles.active} style={styles.tabs}>
+      <Paper style={this.state.open ? styles.container : { display: 'none' }} zDepth={5}>
+        <Tabs
+          contentContainerStyle={styles.tabsContainer}
+          inkBarStyle={styles.active}
+          style={styles.tabs}
+        >
           <Tab label="Route" style={styles.tab}>
             <div style={styles.menu}>
               <AutoComplete
@@ -128,53 +134,60 @@ export default class Menu extends Component {
                 dataSource={this.state.dataSource}
                 onUpdateInput={this.handleUpdate}
                 dataSourceConfig={dataSourceConfig}
-                fullWidth={true} />
+                fullWidth
+              />
 
               {
-                [...Array(this.state.stopovers)].map((stopover, i) => {
-                  return <AutoComplete
-                            key = {i}
-                            floatingLabelText="Over"
-                            onNewRequest={(req, index) => {
-                              this.state.route[stopover + 1] = index === -1 ? '' : this.state.dataSource[index].data.osm_id;
-                            }}
-                            dataSource={this.state.dataSource}
-                            onUpdateInput={this.handleUpdate}
-                            dataSourceConfig={dataSourceConfig}
-                            fullWidth={true} />
-                })
+                [...Array(this.state.stopovers)].map((stopover, i) => (
+                  <AutoComplete
+                    key={i}
+                    floatingLabelText="Over"
+                    onNewRequest={(req, index) => {
+                      this.state.route[stopover + 1] = index === -1 ? '' : this.state.dataSource[index].data.osm_id;
+                    }}
+                    dataSource={this.state.dataSource}
+                    onUpdateInput={this.handleUpdate}
+                    dataSourceConfig={dataSourceConfig}
+                    fullWidth
+                  />
+                ))
               }
 
               <AutoComplete
                 floatingLabelText="To"
                 onNewRequest={(req, index) => {
-                  this.state.route[this.state.stopovers + 1] = index === -1 ? '' : this.state.dataSource[index].data.osm_id
+                  this.state.route[this.state.stopovers + 1] = index === -1 ? '' : this.state.dataSource[index].data.osm_id;
                 }}
                 dataSource={this.state.dataSource}
                 onUpdateInput={this.handleUpdate}
                 dataSourceConfig={dataSourceConfig}
-                fullWidth={true} />
+                fullWidth
+              />
 
               <FlatButton
                 label="Add Stopover"
-                onClick={() => {this.setState({stopovers: this.state.stopovers + 1})}}
-                labelStyle={{textTransform : 'none'}}
-                icon={<FontIcon className="material-icons">add_circle</FontIcon>} />
+                onClick={() => this.setState({ stopovers: this.state.stopovers + 1 })}
+                labelStyle={{ textTransform: 'none' }}
+                icon={<FontIcon className="material-icons">add_circle</FontIcon>}
+              />
 
               <SelectField
                 floatingLabelText="Vehicle"
                 value={this.state.vehicle}
                 onChange={this.vehicleChange}
               >
-                {this.getVehicles().map((vehicle, index) => {
-                  return <MenuItem key={index} value={index} primaryText={vehicle} />
-                })}
+                {this.getVehicles().map((vehicle, index) => (
+                  <MenuItem key={index} value={index} primaryText={vehicle} />
+                ))}
               </SelectField>
 
               <p>Battery Level</p>
               <Slider style={styles.slider} defaultValue={1} />
-              <RaisedButton label="Get Route" onClick={this.getRoute}
-                          icon={<FontIcon className="material-icons">near_me</FontIcon>}/>
+              <RaisedButton
+                label="Get Route"
+                onClick={this.getRoute}
+                icon={<FontIcon className="material-icons">near_me</FontIcon>}
+              />
             </div>
           </Tab>
           <Tab label="Reachability" style={styles.tab}>
@@ -194,7 +207,7 @@ Menu.propTypes = {
   open: PropTypes.bool,
   autoCompleteAddress: PropTypes.string.isRequired,
   getRoute: PropTypes.func.isRequired
-}
+};
 
 Menu.defaultProps = {
   open: true
