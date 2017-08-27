08d5a7b261cc4a3d7e52bdfc283ea80f5d8386da
Ong Jia Rui <ongjiarui@hotmail.com>
2017-08-27 13:41:36 -0700

    Removed GeographicLib and calculateRangePolygonEPSG3857

--- src/reachability.js
+++ src/reachability.js
@@ -1,5 +1,4 @@
 import ol from 'openlayers';
-import GeographicLib from 'geographiclib'; // For geodesic calculations
 import convex from '@turf/convex';
 import { featureCollection, point } from '@turf/helpers';
 
@@ -60,19 +59,4 @@ export const getRangeAnxietyPolygonWithCoordinate = (coord, range) => {
             const hull = convex(featureCollection(pointsArray));
             return hull.geometry.coordinates[0];
           }).catch(() => false);
-};
-
-
-export const calculateRangePolygonEPSG3857 = (range, coord) => {
-  const nCoord = ol.proj.transform(coord, 'EPSG:3857', 'EPSG:4326');
-  const geod = GeographicLib.Geodesic.WGS84;
-  const vertices = [];
-  let angle = 0; // in degrees
-  for (let i = 0; i < 14; i += 1) {
-    const r = geod.Direct(nCoord[1], nCoord[0], angle, range * 1000);
-    angle += 25;
-    const coords = [r.lon2, r.lat2]; // in long lat
-    vertices.push(coords);
-  }
-  return vertices;
 };
